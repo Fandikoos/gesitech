20cb950854e3d4c42f6c25c3bae61531cb896efd
Fandikoos <danfandos@gmail.com>
2023-03-28 10:36:39 +0200

    Creando la estructura del directorio de la webApp y añadiendo los xml correspondientes a WEB-INF y META-INF

--- src/main/java/com/svalero/dao/ProductDAO.java
+++ src/main/java/com/svalero/dao/ProductDAO.java
@@ -2,6 +2,7 @@ package com.svalero.dao;
 
 import com.svalero.domain.Product;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.jdbi.v3.sqlobject.statement.UseRowMapper;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface ProductDAO {
     @SqlQuery("SELECT * FROM PRODUCTOS")
     @UseRowMapper(ProductMbapper.class)
     List<Product> getProducts();
+
+    @SqlUpdate("INSERT INTO productos (ID_PRODUCTO, NOMBRE, STOCK, PRECIO) VALUES (?, ?, ?, ?)")
+    void addProduct (int ID_PRODUCTO, String NOMBRE, int STOCK, int PRECIO);
 }
