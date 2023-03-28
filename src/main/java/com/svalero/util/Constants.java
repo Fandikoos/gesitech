374d3b6771c7d545b771d0fb5bb8f6b74a45ad3b
Fandikoos <danfandos@gmail.com>
2023-03-28 09:54:46 +0200

    Primer commit creando la estructura de la AA, añadiendo las libreria necesarias y formalizando la conexion con mySQL

---
+++ src/main/java/com/svalero/util/Constants.java
@@ -0,0 +1,10 @@
+package com.svalero.util;
+
+public class Constants {
+
+    public static String DATABASE_URL = "jdbc:mysql://localhost:3306/itech";
+
+    public static String DATABASE_USERNAME = "gesitech";
+
+    public static String  DATABASE_PASSWORD = "itech1234";
+}
